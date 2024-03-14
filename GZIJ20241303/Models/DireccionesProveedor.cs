2f290c109e449c5ffb6ffdb85584ad735445d1ae
IGabriel30 <ingridgabriel30@hotmail.com>
2024-03-13 20:13:22 -0600

    Entidades Proveedor y DireccionesProveedor.

---
+++ GZIJ20241303/Models/DireccionesProveedor.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace GZIJ20241303.Models
+{
+    public class DireccionesProveedor
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdDireccion { get; set; }
+        public int ProveedorId { get; set; }
+        [ForeignKey("ProveedorId")]
+        public Proveedor Proveedor { get; set; }
+
+        [Required(ErrorMessage = "El campo Dirección es requerido")]
+        [Display(Name = "Dirección")]
+        [StringLength(200, ErrorMessage = "Máximo 200 carácteres")]
+        public string Direccion { get; set; }
+        [Required(ErrorMessage = "El campo Ciudad es requerido")]
+        [MaxLength(50, ErrorMessage = "Máximo 50 carácteres")]
+        public string Ciudad { get; set; }
+
+        [Display(Name = "País")]
+        [Required(ErrorMessage = "El campo País es requerido")]
+        public string Pais { get; set; }
+
+    }
+}
---
+++ GZIJ20241303/Models/Proveedor.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace GZIJ20241303.Models
+{
+    public class Proveedor
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdProveedor { get; set; }
+
+        [Required(ErrorMessage = "El campo Nombre es requerido")]
+        [StringLength(100, ErrorMessage = "Máximo 100 carácteres")]
+        public string Nombre { get; set; }
+
+
+        [Required(ErrorMessage = "El campo Producto es requerido")]
+        public string Producto { get; set; }
+
+        [Display(Name = "Fecha de registro")]
+        public DateTime FechaRegistro { get; set; }
+
+        [Display(Name = "Teléfono")]
+        [Phone(ErrorMessage = "Digíte un número de teléfono valido")]
+        public string Telefono { get; set; }
+
+        [EmailAddress(ErrorMessage = "Digíte un correo electrónico valido")]
+        [Display(Name = "Correo Electrónico")]
+        [StringLength(200)]
+        public string CorreoElectronico { get; set; }
+
+        public IList<DireccionesProveedor> DireccionesProveedors { get; set; }
+    }
+}
