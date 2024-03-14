cd8183a7f99071d605ffce8eecfa58ba726d7d54
IGabriel30 <ingridgabriel30@hotmail.com>
2024-03-13 20:21:31 -0600

    Clase DBContext.

---
+++ GZIJ20241303/Models/GZIJ20241303DbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GZIJ20241303.Models
+{
+    public class GZIJ20241303DbContext : DbContext
+    {
+        public GZIJ20241303DbContext(DbContextOptions<GZIJ20241303DbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Proveedor> Proveedores { get; set; }
+        public DbSet<DireccionesProveedor> DireccionesProveedors { get; set; }
+
+    }
+}
